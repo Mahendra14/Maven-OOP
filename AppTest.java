cb4c485067250da629f61993a1f4cb4ecd83e285
Raichur Mahendra Kumar <51109700+Mahendra14@users.noreply.github.com>
2020-07-07 12:59:18 +0530

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,18 @@
+package com.epam.maven_OOPS;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
